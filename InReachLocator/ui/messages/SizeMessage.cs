12506fa5a3b0922885967e3678682957a808b9fc
Mark Olbert <mark@arcabama.com>
2022-03-04 10:38:04 -0800

    updated and reorganized messages

---
+++ InReachLocator/ui/messages/SizeMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace J4JSoftware.InReach
+{
+    public record SizeMessage( double Height, double Width )
+    {
+        public SizeMessage( Size size )
+            : this(size.Height, size.Width)
+        {
+        }
+    }
+}
