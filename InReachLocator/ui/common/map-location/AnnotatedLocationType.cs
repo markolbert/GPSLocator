b46ab3514b704b0dc605a5f6bd4824d00cc39bfd
Mark Olbert <mark@arcabama.com>
2022-03-07 13:18:06 -0800

    make MapPoint have an InReach location
    make MapPoint have an InReach location, not be an InReach location

---
+++ InReachLocator/ui/common/map-location/AnnotatedLocationType.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace J4JSoftware.InReach
+{
+    public class AnnotatedLocationType
+    {
+        #region Collection
+
+        public class Choices : IEnumerable<AnnotatedLocationType>
+        {
+            private readonly List<AnnotatedLocationType> _values = new();
+
+            public Choices()
+            {
+                _values.Add(new AnnotatedLocationType(this, LocationType.LinePoint, "Include in Route"));
+                _values.Add(new AnnotatedLocationType(this, LocationType.Pushpin, "Show as Pushpin"));
+                _values.Add(new AnnotatedLocationType(this, LocationType.Unspecified, "Don't Show"));
+            }
+
+            public IEnumerator<AnnotatedLocationType> GetEnumerator()
+            {
+                foreach( var item in _values )
+                {
+                    yield return item;
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        #endregion
+
+        private readonly AnnotatedLocationType.Choices _collection;
+
+        private AnnotatedLocationType(
+            AnnotatedLocationType.Choices collection,
+            LocationType locationType,
+            string? label = null
+        )
+        {
+            _collection = collection;
+
+            LocationType = locationType;
+            Label = label ?? locationType.ToString();
+        }
+
+        public LocationType LocationType { get; }
+        public string Label { get; }
+    }
+}
---
+++ InReachLocator/ui/common/map-location/MapPoint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace J4JSoftware.InReach
+{
+    public class MapPoint : ObservableObject
+    {
+        private AnnotatedLocationType? _selectedLocType;
+
+        public MapPoint(
+            ILocation inReachLocation
+        )
+        {
+            InReachLocation = inReachLocation;
+
+            DisplayPoint = new MapControl.Location( inReachLocation.Coordinate.Latitude,
+                                                inReachLocation.Coordinate.Longitude );
+
+            Label =
+                $"{inReachLocation.Coordinate.Latitude}, {inReachLocation.Coordinate.Longitude}\n{inReachLocation.Timestamp}";
+        }
+
+        public ILocation InReachLocation { get; }
+        public MapControl.Location DisplayPoint { get; }
+
+        public AnnotatedLocationType? SelectedLocationType
+        {
+            get => _selectedLocType;
+            set => SetProperty( ref _selectedLocType, value );
+        }
+
+        public string Label { get; }
+    }
+}
