0c2b56a8c4554b25e2ca4a6b11d4913c20bb00ba
Mark Olbert <mark@arcabama.com>
2022-03-07 13:19:15 -0800

    add visibility converters

---
+++ InReachLocator/ui/common/EmptyCollectionToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+
+namespace J4JSoftware.InReach
+{
+    public class EmptyCollectionToVisibilityConverter : IValueConverter
+    {
+        public object Convert( object? value, Type targetType, object parameter, string language )
+        {
+            if( value is not IList list )
+                return Visibility.Visible;
+
+            return list.Count == 0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack( object value, Type targetType, object parameter, string language ) =>
+            throw new NotImplementedException();
+    }
+}
---
+++ InReachLocator/ui/common/ObjectToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+
+namespace J4JSoftware.InReach
+{
+    public class ObjectToVisibilityConverter : IValueConverter
+    {
+        public object Convert( object? value, Type targetType, object parameter, string language ) =>
+            value == null ? Visibility.Collapsed : Visibility.Visible;
+
+        public object ConvertBack( object value, Type targetType, object parameter, string language ) =>
+            throw new NotImplementedException();
+    }
+}
