5f2fb4e789a91de2cfc1057cc8701a013dc8c595
Mark Olbert <mark@arcabama.com>
2022-03-08 15:48:38 -0800

    simplify AnnotatedLocationType subsystem

--- InReachLocator/ui/common/map-location/AnnotatedLocationType.cs
+++ InReachLocator/ui/common/map-location/AnnotatedLocationType.cs
@@ -11,42 +11,11 @@ namespace J4JSoftware.InReach
 {
     public class AnnotatedLocationType
     {
-        #region Collection
-
-        public class Choices : IEnumerable<AnnotatedLocationType>
-        {
-            private readonly List<AnnotatedLocationType> _values = new();
-
-            public Choices()
-            {
-                _values.Add(new AnnotatedLocationType(this, LocationType.RoutePoint, "Include in Route"));
-                _values.Add(new AnnotatedLocationType(this, LocationType.Pushpin, "Show as Pushpin"));
-                _values.Add(new AnnotatedLocationType(this, LocationType.Unspecified, "Don't Show"));
-            }
-
-            public IEnumerator<AnnotatedLocationType> GetEnumerator()
-            {
-                foreach( var item in _values )
-                {
-                    yield return item;
-                }
-            }
-
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-        }
-
-        #endregion
-
-        private readonly AnnotatedLocationType.Choices _collection;
-
-        private AnnotatedLocationType(
-            AnnotatedLocationType.Choices collection,
+        public AnnotatedLocationType(
             LocationType locationType,
             string? label = null
         )
         {
-            _collection = collection;
-
             LocationType = locationType;
             Label = label ?? locationType.ToString();
         }
