3471e66309109ffaea052d232818a94649e295d0
Mark Olbert <mark@arcabama.com>
2022-03-08 15:47:50 -0800

    remove deprecated components

--- InReachLib/data/LocationMessage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace J4JSoftware.InReach;
-
-public class LocationMessage : Location, ILocationMessage
-{
-    public bool HasMessage => !string.IsNullOrEmpty( Message );
-    public string Message { get; set; } = string.Empty;
-    public List<string> Recipients { get; set; } = new();
-}
--- InReachLocator/ui/common/map-location/IMapLocation.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace J4JSoftware.InReach;
-
-public interface IMapLocation
-{
-    ILocation InReachLocation { get; }
-    MapControl.Location DisplayPoint { get; }
-    LocationType LocationType { get; set; }
-    string Label { get; }
-}
