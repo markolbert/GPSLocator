93c466587a845012c1e05d69269c462d781e705d
Mark Olbert <mark@arcabama.com>
2022-03-14 09:43:06 -0700

    remove as unneeded

--- InReachLib/data/HistoryItem.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.ObjectModel;
-
-namespace J4JSoftware.InReach;
-
-public class HistoryItem : Location
-{
-    public string TextMessage { get; set; } = string.Empty;
-    public ObservableCollection<string> Recipients { get; } = new();
-}
--- InReachLib/data/ILocationMessage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace J4JSoftware.InReach;
-
-public interface ILocationMessage : ILocation
-{
-    bool HasMessage {get; }
-    string Message { get; set; }
-    List<string> Recipients { get; set; }
-}
