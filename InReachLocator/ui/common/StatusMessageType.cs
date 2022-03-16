298c3274038d13221f76683802cbba191b420726
Mark Olbert <mark@arcabama.com>
2022-03-15 19:06:14 -0700

    eliminate unneeded message types

---
+++ InReachLocator/ui/common/StatusMessageType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Messaging;
+using Microsoft.UI.Xaml;
+
+namespace J4JSoftware.InReach
+{
+    public enum StatusMessageType
+    {
+        Normal,
+        Important,
+        Urgent
+    }
+}
