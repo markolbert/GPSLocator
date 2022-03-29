a28294470ab16dc621e201473cee94059a073385
Mark Olbert <mark@arcabama.com>
2022-03-29 14:04:09 -0700

    begin extraction of common UI classes

---
+++ GPSWinUILib/converters/EmptyCollectionToBoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using Microsoft.UI.Xaml.Data;
+
+namespace J4JSoftware.GPSLocator
+{
+    public class EmptyCollectionToBoolConverter : IValueConverter
+    {
+        public object Convert( object? value, Type targetType, object parameter, string language )
+        {
+            if( value is not IList list )
+                return true;
+
+            return list.Count != 0;
+        }
+
+        public object ConvertBack( object value, Type targetType, object parameter, string language ) =>
+            throw new NotImplementedException();
+    }
+}
---
+++ GPSWinUILib/converters/TrueToVisibleConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+
+namespace J4JSoftware.GPSLocator
+{
+    public class TrueToVisibleConverter : IValueConverter
+    {
+        public object Convert( object? value, Type targetType, object parameter, string language )
+        {
+            if( value is not bool flag )
+                return DependencyProperty.UnsetValue;
+
+            return flag ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack( object value, Type targetType, object parameter, string language ) =>
+            throw new NotImplementedException();
+    }
+}
