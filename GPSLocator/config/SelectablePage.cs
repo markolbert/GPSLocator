cb97099cc8ae1de7a344d31c070745715f38504a
Mark Olbert <mark@arcabama.com>
2022-03-30 11:49:03 -0700

    clarify class name

---
+++ GPSLocator/config/SelectablePage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace J4JSoftware.GPSLocator;
+
+public record SelectablePage
+{
+    public SelectablePage(
+        Type? pageType,
+        string pageTag,
+        string label = ""
+    )
+    {
+        PageType = pageType;
+        PageTag = pageTag;
+        Label = string.IsNullOrEmpty(label) ? pageTag : label;
+    }
+
+    public Type? PageType { get; init; } 
+    public string PageTag { get; init; }
+    public string Label { get; init; }
+}
