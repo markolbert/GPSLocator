adbf2d19e0c7e612197353236daa8fafe9d93da1
Mark Olbert <mark@arcabama.com>
2022-03-30 11:59:58 -0700

    remove app-specific stuff

--- GPSLocator/ui/common/viewmodels/AppViewModel.cs
+++ GPSLocator/ui/common/viewmodels/AppViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Text.Json.Serialization;
 using J4JSoftware.Logging;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,19 +9,6 @@ namespace J4JSoftware.GPSLocator;
 
 public class AppViewModel : ObservableObject
 {
-    public static List<SelectablePage> TargetPages { get; } = new()
-    {
-        new SelectablePage( null, ResourceNames.NullPageName ),
-        new SelectablePage( typeof( LastKnownPage ),
-                                  ResourceNames.LastKnownPageName,
-                                  "Last Known Location" ),
-        new SelectablePage( typeof( HistoryPage ), ResourceNames.HistoryPageName, "History" ),
-        new SelectablePage( typeof( MessagingPage ), ResourceNames.MessagingPageName, "Messaging" ),
-        new SelectablePage(typeof(AboutPage), ResourceNames.AboutPageName, "About"),
-        new SelectablePage( typeof( LogViewerPage ), ResourceNames.LogViewerPageName, "Log Viewer" ),
-        new SelectablePage(typeof(SettingsPage), ResourceNames.SettingsPageName, "Settings")
-    };
-
     private string? _statusMesg;
     private Style? _statusStyle;
     private Visibility _determinateVisibility = Visibility.Collapsed;
