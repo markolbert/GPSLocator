7481a1f9b5c3496abd8fdfc8d62f9cc9f25cf0fa
Mark Olbert <mark@arcabama.com>
2022-03-29 20:48:20 -0700

    reorganize

---
+++ GPSLocator/ui/common/user-controls/OpenStreetMapControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Toolkit.Mvvm.Messaging;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace J4JSoftware.GPSLocator;
+
+public sealed partial class OpenStreetMapControl : UserControl
+{
+    public OpenStreetMapControl()
+    {
+        this.InitializeComponent();
+
+        WeakReferenceMessenger.Default.Register<OpenStreetMapControl, MapViewModelMessage, string>(this, "primary", ViewModelChangedHandler);
+    }
+
+    private void ViewModelChangedHandler(OpenStreetMapControl recipient, MapViewModelMessage message )
+    {
+        ViewModel = message.ViewModel;
+    }
+
+    public LocationMapViewModel ViewModel { get; private set; }
+
+    private void UIElement_OnDoubleTapped( object sender, DoubleTappedRoutedEventArgs e )
+    {
+    }
+}
