0422ea1d2a0a32a851c9c02a54f1e2258febad91
Mark Olbert <mark@arcabama.com>
2022-03-30 20:27:27 -0700

    setting up GPSRouter

---
+++ GPSRouter/config/AppConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using J4JSoftware.GPSCommon;
+using Serilog.Events;
+
+namespace J4JSoftware.GPSLocator;
+
+public class AppConfig : BaseAppConfig
+{
+    private bool _useImperial;
+    private bool _useCompass;
+    private int _defaultDaysBack;
+
+    public bool UseImperialUnits
+    {
+        get=> _useImperial;
+        set => SetProperty( ref _useImperial, value );
+    }
+
+    public bool UseCompassHeadings
+    {
+        get => _useCompass;
+        set=> SetProperty( ref _useCompass, value);
+    }
+
+    public int DefaultDaysBack
+    {
+        get => _defaultDaysBack;
+        set => SetProperty( ref _defaultDaysBack, value );
+    }
+}
---
+++ docs/nuget-router.md
@@ -0,0 +1,9 @@
+# GPSRouter
+
+A Windows desktop app for creating routes from GPS devices. Currently supports
+Garmin's InReach.
+
+Licensed under GNU GPL-v3.0.
+
+See the [github documentation](https://github.com/markolbert/GPSLocator) for more detail.
+
